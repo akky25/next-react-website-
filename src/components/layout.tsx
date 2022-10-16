791ea5289e5f6addab893f422743d6640b5c0982
akky25 <ry.akita1991@gmail.com>
2022-10-16 20:40:51 +0900

    Layout Component 追加

---
+++ src/components/layout.tsx
@@ -0,0 +1,16 @@
+import { ReactElement } from 'react';
+import Footer from './footer';
+import Header from './header';
+
+type Props = {
+  children: ReactElement;
+};
+export default function Layout({ children }: Props) {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,8 @@
 import type { NextPage } from 'next';
-import Footer from '@/components/footer';
-import Header from '@/components/header';
 import Hero from '@/components/hero';
 
 const Home: NextPage = () => {
-  return (
-    <>
-      <header>HEADER</header>
-      <main>
-        <>
-          <Header />
-          <main>
-            <Hero />
-          </main>
-          <Footer />
-        </>
-      </main>
-      <footer>FOOTER</footer>
-    </>
-  );
+  return <Hero />;
 };
 
 export default Home;
