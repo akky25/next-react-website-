ce56cd6b97e0761cf091a4666c9ac5b19f4117e4
akky25 <ry.akita1991@gmail.com>
2022-10-22 10:47:13 +0900

    HEROコンポーネントのPorpsに画像フラグを追加

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,13 +1,19 @@
 type Props = {
   title: string;
   subtitle: string;
+  imageOn?: boolean;
 };
 
-export default function Hero({ title, subtitle }: Props) {
+export default function Hero({
+  title,
+  subtitle,
+  imageOn: imageOn = false,
+}: Props) {
   return (
     <div>
       <h1>{title}</h1>
       <p>{subtitle}</p>
+      {imageOn && <figure>［画像］</figure>}
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next';
 import Hero from '@/components/hero';
 
 const Home: NextPage = () => {
-  return <Hero title="CUBE" subtitle="アウトプットしていくサイト" />;
+  return <Hero title="CUBE" subtitle="アウトプットしていくサイト" imageOn />;
 };
 
 export default Home;
