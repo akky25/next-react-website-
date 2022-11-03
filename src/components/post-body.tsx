d7358b95b2c66b3a270a53e389efbf1c803073d3
akky25 <ry.akita1991@gmail.com>
2022-11-04 08:42:35 +0900

    PostBodyレイアウトのスタイル追加

---
+++ src/components/post-body.tsx
@@ -0,0 +1,6 @@
+import { ReactNode } from 'react';
+import styles from '@/styles/post-body.module.css';
+
+export default function PostBody({ children }: { children: ReactNode }) {
+  return <div className={styles.stack}>{children}</div>;
+}
