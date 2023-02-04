631376e3697dafaf76f223e3f8ef7b520ef9e26f
akky25 <ry.akita1991@gmail.com>
2023-02-04 21:28:42 +0900

    ハンバーガーメニュー追加

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,23 +1,36 @@
 import Link from 'next/link';
+import { useState } from 'react';
 import styles from '@/styles/nav.module.css';
 
 export default function Nav() {
+  const [navIsOpen, setNavIsOpen] = useState(false);
+
+  const toggleNav = () => setNavIsOpen((p) => !p);
+
+  const closeNav = () => {
+    setNavIsOpen(false);
+  };
+
   return (
-    <nav>
+    <nav className={navIsOpen ? styles.open : styles.close}>
+      <button className={styles.btn} onClick={toggleNav}>
+        <span className={styles.bar} />
+        <span className="sr-only">MENU</span>
+      </button>
       <ul className={styles.list}>
         <li>
           <Link href="/">
-            <a>Home</a>
+            <a onClick={closeNav}>Home</a>
           </Link>
         </li>
         <li>
           <Link href="/about">
-            <a>About</a>
+            <a onClick={closeNav}>About</a>
           </Link>
         </li>
         <li>
           <Link href="/blog">
-            <a>Blog</a>
+            <a onClick={closeNav}>Blog</a>
           </Link>
         </li>
       </ul>
